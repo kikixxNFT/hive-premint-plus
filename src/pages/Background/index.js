4a4af0f1fe14b107e8deb18843bbf2797d000097
kikixxNFT <104740009+kikixxNFT@users.noreply.github.com>
2022-05-06 20:40:04 +0100

    background script changes to validate wallet holder

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -10,11 +10,30 @@ const limit = RateLimit(2);
 const provider = new ethers.providers.JsonRpcProvider(rpc);
 const contractInstance = new ethers.Contract(contractAddress, abi, provider);
 
+async function hasPasses({ address }) {
+  const foundersPasses = await contractInstance.balanceOf(address, 1);
+  const alphaPasses = await contractInstance.balanceOf(address, 2);
+  return foundersPasses.add(alphaPasses).gt(BigNumber.from(0));
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.raffles) {
     setBadgeText({ raffles: request.raffles });
+    sendResponse({ badgeUpdated: true });
+  } else if (request.verifyAddress) {
+    const address = ethers.utils.verifyMessage(
+      request.nonce.toString(),
+      request.signature
+    );
+    const matchedAddress =
+      request.account.toLowerCase() === address.toLowerCase();
+    hasPasses({ address }).then((walletHasPasses) =>
+      sendResponse({ authenticated: matchedAddress && walletHasPasses })
+    );
+    return true;
+  } else {
+    sendResponse({ error: 'unknown request' });
   }
-  sendResponse({ updated: true });
 });
 
 chrome.runtime.onInstalled.addListener((reason) => {
@@ -34,49 +53,46 @@ chrome.alarms.onAlarm.addListener(() => {
     const { raffles, wallet, interval, autoDeleteLost } = data?.[SETTINGS_KEY];
     if (!wallet) return;
 
-    const foundersPasses = await contractInstance.balanceOf(wallet, 1);
-    const alphaPasses = await contractInstance.balanceOf(wallet, 2);
-    /*if (!foundersPasses.add(alphaPasses).gt(BigNumber.from(0))) {
-        const newSettings = produce(data?.[SETTINGS_KEY], (draft) => {
-          delete draft.wallet
-        })
-        chrome.storage.sync.set({ [SETTINGS_KEY]: newSettings })
-      } else {*/
-    const updatedRaffles = Object.entries(raffles).filter(
-      ([, data]) => Date.now() - data?.updated_at >= interval * 60 * 1000
-    );
-    if (updatedRaffles.length > 0) {
-      updatedRaffles.map(async ([url]) => {
-        await limit();
-        const res = await fetch(`${url}/verify/?wallet=${wallet}`, {
-          method: 'GET',
-          mode: 'cors',
-        });
-        const txt = await res.text();
-        let matchedStatus = 'unknown';
-        const newSettings = produce(data?.[SETTINGS_KEY], (draft) => {
-          if (txt.includes(UNREGISTERED.wording)) {
-            matchedStatus = UNREGISTERED.status;
-          } else {
-            for (let [key, status] of Object.entries(statuses)) {
-              if (txt.includes(key)) {
-                matchedStatus = status;
-                break;
+    if (!hasPasses({ address: wallet })) {
+      const newSettings = produce(data?.[SETTINGS_KEY], (draft) => {
+        delete draft.wallet;
+      });
+      chrome.storage.sync.set({ [SETTINGS_KEY]: newSettings });
+    } else {
+      const updatedRaffles = Object.entries(raffles).filter(
+        ([, data]) => Date.now() - data?.updated_at >= interval * 60 * 1000
+      );
+      if (updatedRaffles.length > 0) {
+        updatedRaffles.map(async ([url]) => {
+          await limit();
+          const res = await fetch(`${url}/verify/?wallet=${wallet}`, {
+            method: 'GET',
+            mode: 'cors',
+          });
+          const txt = await res.text();
+          let matchedStatus = 'unknown';
+          const newSettings = produce(data?.[SETTINGS_KEY], (draft) => {
+            if (txt.includes(UNREGISTERED.wording)) {
+              matchedStatus = UNREGISTERED.status;
+            } else {
+              for (let [key, status] of Object.entries(statuses)) {
+                if (txt.includes(key)) {
+                  matchedStatus = status;
+                  break;
+                }
               }
             }
-          }
-          draft.raffles[url].status = matchedStatus;
-          draft.raffles[url].updated_at = Date.now();
+            draft.raffles[url].status = matchedStatus;
+            draft.raffles[url].updated_at = Date.now();
 
-          if (autoDeleteLost && matchedStatus === 'lost') {
-            delete draft.raffles[url];
-          }
+            if (autoDeleteLost && matchedStatus === 'lost') {
+              delete draft.raffles[url];
+            }
+          });
+          chrome.storage.sync.set({ [SETTINGS_KEY]: newSettings });
         });
-        console.log(`${Date.now()}: ${url}`, newSettings);
-        chrome.storage.sync.set({ [SETTINGS_KEY]: newSettings });
-      });
+      }
     }
-    //}
   });
 
   chrome.storage.sync.get(SETTINGS_KEY, (data) => {
